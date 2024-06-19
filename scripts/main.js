748f1c2ce5322c8234939bc20f820a80aaf5e59f
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-19 06:29:17 +0200

    feat(teamMember): object class made to save in an object all the data of the arrays

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,11 @@
 const teamMembersNames = ['Wayne Barnett' , 'Angela Caroll' , 'Walter Gordon' , 'Angela Lopez' , 'Scott Estrada' , 'Barbara Ramos'];
 const teamMembersRoles = ['Founder &amp CEO' , 'Chief Editor' , 'Office Manager' , 'Social Media Manager' , 'Developer' , 'Graphic Designer'];
 const teamMembersImages = ['wayne-barnett-founder-ceo.jpg' , 'angela-caroll-chief-editor.jpg' , 'walter-gordon-office-manager.jpg' , 'angela-lopez-social-media-manager.jpg' , 'scott-estrada-developer.jpg' , 'barbara-ramos-graphic-designer.jpg'];
+
+// Class
+class teamMember {
+    constructor(name , role , image) {
+        this.name = name;
+        this.role = role;
+        this.image = image;
+    }
