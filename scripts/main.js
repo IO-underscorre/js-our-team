3cd435afd8f42b8677a0cb8a3c460b407abad801
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-19 06:27:40 +0200

    build(js): added placeholder row data

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+// Inputs
+const teamMembersNames = ['Wayne Barnett' , 'Angela Caroll' , 'Walter Gordon' , 'Angela Lopez' , 'Scott Estrada' , 'Barbara Ramos'];
+const teamMembersRoles = ['Founder &amp CEO' , 'Chief Editor' , 'Office Manager' , 'Social Media Manager' , 'Developer' , 'Graphic Designer'];
+const teamMembersImages = ['wayne-barnett-founder-ceo.jpg' , 'angela-caroll-chief-editor.jpg' , 'walter-gordon-office-manager.jpg' , 'angela-lopez-social-media-manager.jpg' , 'scott-estrada-developer.jpg' , 'barbara-ramos-graphic-designer.jpg'];
